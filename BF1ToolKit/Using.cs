cb20f43a9fe28095ebb7c469b0d9952706fc4e14
穗乃果 <1763575579@qq.com>
2022-09-07 03:28:52 +0800

    update

--- BF1ToolKit/Using.cs
+++ BF1ToolKit/Using.cs
@@ -3,3 +3,6 @@ global using System.Windows;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.ComponentModel;
+global using System.Windows.Controls;
+global using System.Windows.Input;
+global using System.Windows.Navigation;
--- BF1ToolKit/Views/AboutView.xaml.cs
+++ BF1ToolKit/Views/AboutView.xaml.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using BF1ToolKit.Util;
 
-namespace BF1ToolKit.Views
+namespace BF1ToolKit.Views;
+
+/// <summary>
+/// AboutView.xaml 的交互逻辑
+/// </summary>
+public partial class AboutView : UserControl
 {
-    /// <summary>
-    /// AboutView.xaml 的交互逻辑
-    /// </summary>
-    public partial class AboutView : UserControl
+    public AboutView()
     {
-        public AboutView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        ProcessUtil.OpenLink(e.Uri.OriginalString);
+        e.Handled = true;
+    }
+
+    private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        ProcessUtil.OpenLink("https://afdian.net/@crazyzhang");
     }
 }
